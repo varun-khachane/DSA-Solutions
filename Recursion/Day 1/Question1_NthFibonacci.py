5b9d2e1129ab8b567286aac72ae6971c35c86853
varun-khachane <46482922+varun-khachane@users.noreply.github.com>
2021-06-21 10:45:45 +0530

    trial with visual studio pushing solutions

---
+++ Recursion/Day 1/Question1_NthFibonacci.py
@@ -0,0 +1,11 @@
+# Question 1
+# Nth Fibonacci
+
+def fibonacci(n):
+    if n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
